619ad856ca460951e4a16d31624e19313eb3f371
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-12-11 21:13:17 -0800

    auth System

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const flash = require('express-flash')
 
 const MongoDbStore = require('connect-mongo')(session)
 
+const passport = require('passport')
+
 //Database connection
 
 const url = 'mongodb://localhost:27017/pizza';
@@ -47,18 +49,26 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash())
 
 //Assets
 
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Global middleware
 
 app.use((req, res, next) => {
 res.locals.session = req.session
+res.locals.user = req.user
 next()
 })
 
